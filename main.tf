eb0d8c9999abbd19e1d5788d01b6b75215e6449b
User <user@Student.local>
2020-01-22 16:09:20 +0900

    added tags

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-
 terraform {
   required_version = "~> 0.12"
 }
@@ -13,5 +12,8 @@ resource "aws_instance" "hello-tf-instance" {
   ami = var.ami
   count = var.hello_tf_instance_count
   instance_type = var.hello_tf_instance_type
+  tags = {
+    owner = "Kabu"
+    ttl   = "100"
+  }
 }
-
